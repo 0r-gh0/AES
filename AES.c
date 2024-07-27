0706fc46d392800b4b29ebf7594e94fee0172407
Argha <arghacious@gmail.com>
2024-07-27 20:54:11 +0530

    Pares_test

--- AES.c
+++ AES.c
@@ -49,14 +49,17 @@ void parseHexWords(const char *hexString, HexWord *wordArray, unsigned char len)
 
 int main(){
 
+    const char *key = "e2fc70d2"; // Example input string
+    unsigned char keyLen = 8;
+
     // const char *key = "2b7e151628aed2a6abf7158809cf4f3c"; // Example input string
     // unsigned char keyLen = 32;
 
     // const char *key = "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b"; // Example input string
     // unsigned char keyLen = 48;
 
-    const char *key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"; // Example input string
-    unsigned char keyLen = 64;
+    // const char *key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"; // Example input string
+    // unsigned char keyLen = 64;
 
     unsigned char keyCount = keyLen/8;
     HexWord keyArray[keyCount];
