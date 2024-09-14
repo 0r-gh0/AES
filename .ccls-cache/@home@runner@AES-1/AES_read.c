52e1dbf4084c47fc84877c15b8ae7f76672387f1
Argha <92508807+0r-gh0@users.noreply.github.com>
2024-09-14 19:45:15 +0000

    CFB Encryption + Decryption
    CFB Enc and Dec Done and matched

--- .ccls-cache/@home@runner@AES-1/AES_read.c
+++ .ccls-cache/@home@runner@AES-1/AES_read.c
@@ -542,7 +542,7 @@ int main() {
 
   char read_block;
   int pad_counter = 0;
-  iFile = fopen("hi.txt", "rb");
+  iFile = fopen("hi_2.txt", "rb");
   oFile = fopen("encrypt.bin", "wb");
 
   if (iFile == NULL || oFile == NULL) {
@@ -1278,7 +1278,7 @@ printf("\n\nDECRYPTED !!!\n"); */
     */
 
     // OFB
-
+/*
     input_2[0] = IV[0];
     input_2[1] = IV[1];
     input_2[2] = IV[2];
@@ -1601,6 +1601,138 @@ printf("\n\nDECRYPTED !!!\n"); */
         fwrite(out_buff, 1, 16, oFile);
       }
 
+    for (int i = 14; i >= 0; i--) {
+        if ((int)out_buff[i] == (int)out_buff[15]) {
+          pad_counter++;
+        }
+      }
+
+      if (pad_counter + 1 == (int)out_buff[15]) {
+        fseek(oFile, 0, SEEK_END);       // Seek to the End of the file
+        long currentSize = ftell(oFile); // Determine the new File Size
+        long newSize = currentSize - (int)out_buff[15];
+        ftruncate(fileno(oFile), newSize); // Truncate the File
+      }
+
+      fclose(iFile);
+      fclose(oFile);
+      printf("\n\nDECRYPTED !!!\n");
+*/
+    // CFB 
+
+    input_2[0] = IV[0];
+    input_2[1] = IV[1];
+    input_2[2] = IV[2];
+    input_2[3] = IV[3];
+
+    printf("\n~ Encryption ~\n");
+      while ((byteRead = fread(buff, 1, 16, iFile)) == 16) {
+        in[0].bytes[0].nibbles.low = buff[0];
+        in[0].bytes[0].nibbles.high = buff[0] >> 4;
+        in[0].bytes[1].nibbles.low = buff[1];
+        in[0].bytes[1].nibbles.high = buff[1] >> 4;
+        in[0].bytes[2].nibbles.low = buff[2];
+        in[0].bytes[2].nibbles.high = buff[2] >> 4;
+        in[0].bytes[3].nibbles.low = buff[3];
+        in[0].bytes[3].nibbles.high = buff[3] >> 4;
+
+        in[1].bytes[0].nibbles.low = buff[4];
+        in[1].bytes[0].nibbles.high = buff[4] >> 4;
+        in[1].bytes[1].nibbles.low = buff[5];
+        in[1].bytes[1].nibbles.high = buff[5] >> 4;
+        in[1].bytes[2].nibbles.low = buff[6];
+        in[1].bytes[2].nibbles.high = buff[6] >> 4;
+        in[1].bytes[3].nibbles.low = buff[7];
+        in[1].bytes[3].nibbles.high = buff[7] >> 4;
+
+        in[2].bytes[0].nibbles.low = buff[8];
+        in[2].bytes[0].nibbles.high = buff[8] >> 4;
+        in[2].bytes[1].nibbles.low = buff[9];
+        in[2].bytes[1].nibbles.high = buff[9] >> 4;
+        in[2].bytes[2].nibbles.low = buff[10];
+        in[2].bytes[2].nibbles.high = buff[10] >> 4;
+        in[2].bytes[3].nibbles.low = buff[11];
+        in[2].bytes[3].nibbles.high = buff[11] >> 4;
+
+        in[3].bytes[0].nibbles.low = buff[12];
+        in[3].bytes[0].nibbles.high = buff[12] >> 4;
+        in[3].bytes[1].nibbles.low = buff[13];
+        in[3].bytes[1].nibbles.high = buff[13] >> 4;
+        in[3].bytes[2].nibbles.low = buff[14];
+        in[3].bytes[2].nibbles.high = buff[14] >> 4;
+        in[3].bytes[3].nibbles.low = buff[15];
+        in[3].bytes[3].nibbles.high = buff[15] >> 4;
+
+        Encrypt(input_2, keyScheduling, output);
+
+        output[0].bytes[0].byte = output[0].bytes[0].byte ^ in[0].bytes[0].byte;
+        output[0].bytes[1].byte = output[0].bytes[1].byte ^ in[0].bytes[1].byte;
+        output[0].bytes[2].byte = output[0].bytes[2].byte ^ in[0].bytes[2].byte;
+        output[0].bytes[3].byte = output[0].bytes[3].byte ^ in[0].bytes[3].byte;
+
+        output[1].bytes[0].byte = output[1].bytes[0].byte ^ in[1].bytes[0].byte;
+        output[1].bytes[1].byte = output[1].bytes[1].byte ^ in[1].bytes[1].byte;
+        output[1].bytes[2].byte = output[1].bytes[2].byte ^ in[1].bytes[2].byte;
+        output[1].bytes[3].byte = output[1].bytes[3].byte ^ in[1].bytes[3].byte;
+
+        output[2].bytes[0].byte = output[2].bytes[0].byte ^ in[2].bytes[0].byte;
+        output[2].bytes[1].byte = output[2].bytes[1].byte ^ in[2].bytes[1].byte;
+        output[2].bytes[2].byte = output[2].bytes[2].byte ^ in[2].bytes[2].byte;
+        output[2].bytes[3].byte = output[2].bytes[3].byte ^ in[2].bytes[3].byte;
+
+        output[3].bytes[0].byte = output[3].bytes[0].byte ^ in[3].bytes[0].byte;
+        output[3].bytes[1].byte = output[3].bytes[1].byte ^ in[3].bytes[1].byte;
+        output[3].bytes[2].byte = output[3].bytes[2].byte ^ in[3].bytes[2].byte;
+        output[3].bytes[3].byte = output[3].bytes[3].byte ^ in[3].bytes[3].byte;
+
+        for (unsigned char i = 0; i < 4; i++) {
+          printHexWord(output[i]);
+        }
+          
+          input_2[0] =  output[0];
+          input_2[1] =  output[1];
+          input_2[2] =  output[2];
+          input_2[3] =  output[3];
+
+        out_buff[0] =
+            output[0].bytes[0].nibbles.high << 4 | output[0].bytes[0].nibbles.low;
+        out_buff[1] =
+            output[0].bytes[1].nibbles.high << 4 | output[0].bytes[1].nibbles.low;
+        out_buff[2] =
+            output[0].bytes[2].nibbles.high << 4 | output[0].bytes[2].nibbles.low;
+        out_buff[3] =
+            output[0].bytes[3].nibbles.high << 4 | output[0].bytes[3].nibbles.low;
+
+        out_buff[4] =
+            output[1].bytes[0].nibbles.high << 4 | output[1].bytes[0].nibbles.low;
+        out_buff[5] =
+            output[1].bytes[1].nibbles.high << 4 | output[1].bytes[1].nibbles.low;
+        out_buff[6] =
+            output[1].bytes[2].nibbles.high << 4 | output[1].bytes[2].nibbles.low;
+        out_buff[7] =
+            output[1].bytes[3].nibbles.high << 4 | output[1].bytes[3].nibbles.low;
+
+        out_buff[8] =
+            output[2].bytes[0].nibbles.high << 4 | output[2].bytes[0].nibbles.low;
+        out_buff[9] =
+            output[2].bytes[1].nibbles.high << 4 | output[2].bytes[1].nibbles.low;
+        out_buff[10] =
+            output[2].bytes[2].nibbles.high << 4 | output[2].bytes[2].nibbles.low;
+        out_buff[11] =
+            output[2].bytes[3].nibbles.high << 4 | output[2].bytes[3].nibbles.low;
+
+        out_buff[12] =
+            output[3].bytes[0].nibbles.high << 4 | output[3].bytes[0].nibbles.low;
+        out_buff[13] =
+            output[3].bytes[1].nibbles.high << 4 | output[3].bytes[1].nibbles.low;
+        out_buff[14] =
+            output[3].bytes[2].nibbles.high << 4 | output[3].bytes[2].nibbles.low;
+        out_buff[15] =
+            output[3].bytes[3].nibbles.high << 4 | output[3].bytes[3].nibbles.low;
+
+        fwrite(out_buff, 1, 16, oFile);
+      }
+
       while (tempCounter < byteRead) {
         if (temp_j == 4) {
           temp_j = 0;
@@ -1687,152 +1819,144 @@ printf("\n\nDECRYPTED !!!\n"); */
 
       fclose(iFile);
       fclose(oFile);
-      printf("\n\nDECRYPTED !!!\n\n");
+      printf("\n\nENCRYPTED !!!\n\n");
 
+      iFile = fopen("encrypt.bin", "rb");
+      oFile = fopen("decrypt.bin", "wb");
 
-    //////////////////////END END END END END END ///////////////////
+      if (iFile == NULL || oFile == NULL) {
+        printf("File Couldn't be opened !!");
+        return 1;
+      }
 
-      // input_2[0] = IV[0];
-      // input_2[1] = IV[1];
-      // input_2[2] = IV[2];
-      // input_2[3] = IV[3];
+    input_2[0] = IV[0];
+    input_2[1] = IV[1];
+    input_2[2] = IV[2];
+    input_2[3] = IV[3];
 
-      // while ((byteRead = fread(buff, 1, 16, iFile)) == 16) {
-      //   in[0].bytes[0].nibbles.low = buff[0];
-      //   in[0].bytes[0].nibbles.high = buff[0] >> 4;
-      //   in[0].bytes[1].nibbles.low = buff[1];
-      //   in[0].bytes[1].nibbles.high = buff[1] >> 4;
-      //   in[0].bytes[2].nibbles.low = buff[2];
-      //   in[0].bytes[2].nibbles.high = buff[2] >> 4;
-      //   in[0].bytes[3].nibbles.low = buff[3];
-      //   in[0].bytes[3].nibbles.high = buff[3] >> 4;
+    printf("\n~ Decryption ~\n");
+      while ((byteRead = fread(buff, 1, 16, iFile)) == 16) {
+        in[0].bytes[0].nibbles.low = buff[0];
+        in[0].bytes[0].nibbles.high = buff[0] >> 4;
+        in[0].bytes[1].nibbles.low = buff[1];
+        in[0].bytes[1].nibbles.high = buff[1] >> 4;
+        in[0].bytes[2].nibbles.low = buff[2];
+        in[0].bytes[2].nibbles.high = buff[2] >> 4;
+        in[0].bytes[3].nibbles.low = buff[3];
+        in[0].bytes[3].nibbles.high = buff[3] >> 4;
 
-      //   in[1].bytes[0].nibbles.low = buff[4];
-      //   in[1].bytes[0].nibbles.high = buff[4] >> 4;
-      //   in[1].bytes[1].nibbles.low = buff[5];
-      //   in[1].bytes[1].nibbles.high = buff[5] >> 4;
-      //   in[1].bytes[2].nibbles.low = buff[6];
-      //   in[1].bytes[2].nibbles.high = buff[6] >> 4;
-      //   in[1].bytes[3].nibbles.low = buff[7];
-      //   in[1].bytes[3].nibbles.high = buff[7] >> 4;
+        in[1].bytes[0].nibbles.low = buff[4];
+        in[1].bytes[0].nibbles.high = buff[4] >> 4;
+        in[1].bytes[1].nibbles.low = buff[5];
+        in[1].bytes[1].nibbles.high = buff[5] >> 4;
+        in[1].bytes[2].nibbles.low = buff[6];
+        in[1].bytes[2].nibbles.high = buff[6] >> 4;
+        in[1].bytes[3].nibbles.low = buff[7];
+        in[1].bytes[3].nibbles.high = buff[7] >> 4;
 
-      //   in[2].bytes[0].nibbles.low = buff[8];
-      //   in[2].bytes[0].nibbles.high = buff[8] >> 4;
-      //   in[2].bytes[1].nibbles.low = buff[9];
-      //   in[2].bytes[1].nibbles.high = buff[9] >> 4;
-      //   in[2].bytes[2].nibbles.low = buff[10];
-      //   in[2].bytes[2].nibbles.high = buff[10] >> 4;
-      //   in[2].bytes[3].nibbles.low = buff[11];
-      //   in[2].bytes[3].nibbles.high = buff[11] >> 4;
+        in[2].bytes[0].nibbles.low = buff[8];
+        in[2].bytes[0].nibbles.high = buff[8] >> 4;
+        in[2].bytes[1].nibbles.low = buff[9];
+        in[2].bytes[1].nibbles.high = buff[9] >> 4;
+        in[2].bytes[2].nibbles.low = buff[10];
+        in[2].bytes[2].nibbles.high = buff[10] >> 4;
+        in[2].bytes[3].nibbles.low = buff[11];
+        in[2].bytes[3].nibbles.high = buff[11] >> 4;
 
-      //   in[3].bytes[0].nibbles.low = buff[12];
-      //   in[3].bytes[0].nibbles.high = buff[12] >> 4;
-      //   in[3].bytes[1].nibbles.low = buff[13];
-      //   in[3].bytes[1].nibbles.high = buff[13] >> 4;
-      //   in[3].bytes[2].nibbles.low = buff[14];
-      //   in[3].bytes[2].nibbles.high = buff[14] >> 4;
-      //   in[3].bytes[3].nibbles.low = buff[15];
-      //   in[3].bytes[3].nibbles.high = buff[15] >> 4;
+        in[3].bytes[0].nibbles.low = buff[12];
+        in[3].bytes[0].nibbles.high = buff[12] >> 4;
+        in[3].bytes[1].nibbles.low = buff[13];
+        in[3].bytes[1].nibbles.high = buff[13] >> 4;
+        in[3].bytes[2].nibbles.low = buff[14];
+        in[3].bytes[2].nibbles.high = buff[14] >> 4;
+        in[3].bytes[3].nibbles.low = buff[15];
+        in[3].bytes[3].nibbles.high = buff[15] >> 4;
 
-      //   Decrypt(in, keyScheduling, output);
+        Encrypt(input_2, keyScheduling, output);
 
-      //   output[0].bytes[0].byte =
-      //       input_2[0].bytes[0].byte ^ output[0].bytes[0].byte;
-      //   output[0].bytes[1].byte =
-      //       input_2[0].bytes[1].byte ^ output[0].bytes[1].byte;
-      //   output[0].bytes[2].byte =
-      //       input_2[0].bytes[2].byte ^ output[0].bytes[2].byte;
-      //   output[0].bytes[3].byte =
-      //       input_2[0].bytes[3].byte ^ output[0].bytes[3].byte;
+        input_2[0] =  in[0];
+        input_2[1] =  in[1];
+        input_2[2] =  in[2];
+        input_2[3] =  in[3];
 
-      //   output[1].bytes[0].byte =
-      //       input_2[1].bytes[0].byte ^ output[1].bytes[0].byte;
-      //   output[1].bytes[1].byte =
-      //       input_2[1].bytes[1].byte ^ output[1].bytes[1].byte;
-      //   output[1].bytes[2].byte =
-      //       input_2[1].bytes[2].byte ^ output[1].bytes[2].byte;
-      //   output[1].bytes[3].byte =
-      //       input_2[1].bytes[3].byte ^ output[1].bytes[3].byte;
+        output[0].bytes[0].byte = output[0].bytes[0].byte ^ in[0].bytes[0].byte;
+        output[0].bytes[1].byte = output[0].bytes[1].byte ^ in[0].bytes[1].byte;
+        output[0].bytes[2].byte = output[0].bytes[2].byte ^ in[0].bytes[2].byte;
+        output[0].bytes[3].byte = output[0].bytes[3].byte ^ in[0].bytes[3].byte;
 
-      //   output[2].bytes[0].byte =
-      //       input_2[2].bytes[0].byte ^ output[2].bytes[0].byte;
-      //   output[2].bytes[1].byte =
-      //       input_2[2].bytes[1].byte ^ output[2].bytes[1].byte;
-      //   output[2].bytes[2].byte =
-      //       input_2[2].bytes[2].byte ^ output[2].bytes[2].byte;
-      //   output[2].bytes[3].byte =
-      //       input_2[2].bytes[3].byte ^ output[2].bytes[3].byte;
+        output[1].bytes[0].byte = output[1].bytes[0].byte ^ in[1].bytes[0].byte;
+        output[1].bytes[1].byte = output[1].bytes[1].byte ^ in[1].bytes[1].byte;
+        output[1].bytes[2].byte = output[1].bytes[2].byte ^ in[1].bytes[2].byte;
+        output[1].bytes[3].byte = output[1].bytes[3].byte ^ in[1].bytes[3].byte;
 
-      //   output[3].bytes[0].byte =
-      //       input_2[3].bytes[0].byte ^ output[3].bytes[0].byte;
-      //   output[3].bytes[1].byte =
-      //       input_2[3].bytes[1].byte ^ output[3].bytes[1].byte;
-      //   output[3].bytes[2].byte =
-      //       input_2[3].bytes[2].byte ^ output[3].bytes[2].byte;
-      //   output[3].bytes[3].byte =
-      //       input_2[3].bytes[3].byte ^ output[3].bytes[3].byte;
+        output[2].bytes[0].byte = output[2].bytes[0].byte ^ in[2].bytes[0].byte;
+        output[2].bytes[1].byte = output[2].bytes[1].byte ^ in[2].bytes[1].byte;
+        output[2].bytes[2].byte = output[2].bytes[2].byte ^ in[2].bytes[2].byte;
+        output[2].bytes[3].byte = output[2].bytes[3].byte ^ in[2].bytes[3].byte;
 
-      //   out_buff[0] =
-      //       output[0].bytes[0].nibbles.high << 4 | output[0].bytes[0].nibbles.low;
-      //   out_buff[1] =
-      //       output[0].bytes[1].nibbles.high << 4 | output[0].bytes[1].nibbles.low;
-      //   out_buff[2] =
-      //       output[0].bytes[2].nibbles.high << 4 | output[0].bytes[2].nibbles.low;
-      //   out_buff[3] =
-      //       output[0].bytes[3].nibbles.high << 4 | output[0].bytes[3].nibbles.low;
+        output[3].bytes[0].byte = output[3].bytes[0].byte ^ in[3].bytes[0].byte;
+        output[3].bytes[1].byte = output[3].bytes[1].byte ^ in[3].bytes[1].byte;
+        output[3].bytes[2].byte = output[3].bytes[2].byte ^ in[3].bytes[2].byte;
+        output[3].bytes[3].byte = output[3].bytes[3].byte ^ in[3].bytes[3].byte;
 
-      //   out_buff[4] =
-      //       output[1].bytes[0].nibbles.high << 4 | output[1].bytes[0].nibbles.low;
-      //   out_buff[5] =
-      //       output[1].bytes[1].nibbles.high << 4 | output[1].bytes[1].nibbles.low;
-      //   out_buff[6] =
-      //       output[1].bytes[2].nibbles.high << 4 | output[1].bytes[2].nibbles.low;
-      //   out_buff[7] =
-      //       output[1].bytes[3].nibbles.high << 4 | output[1].bytes[3].nibbles.low;
+        for (unsigned char i = 0; i < 4; i++) {
+          printHexWord(output[i]);
+        }
 
-      //   out_buff[8] =
-      //       output[2].bytes[0].nibbles.high << 4 | output[2].bytes[0].nibbles.low;
-      //   out_buff[9] =
-      //       output[2].bytes[1].nibbles.high << 4 | output[2].bytes[1].nibbles.low;
-      //   out_buff[10] =
-      //       output[2].bytes[2].nibbles.high << 4 | output[2].bytes[2].nibbles.low;
-      //   out_buff[11] =
-      //       output[2].bytes[3].nibbles.high << 4 | output[2].bytes[3].nibbles.low;
+        out_buff[0] =
+            output[0].bytes[0].nibbles.high << 4 | output[0].bytes[0].nibbles.low;
+        out_buff[1] =
+            output[0].bytes[1].nibbles.high << 4 | output[0].bytes[1].nibbles.low;
+        out_buff[2] =
+            output[0].bytes[2].nibbles.high << 4 | output[0].bytes[2].nibbles.low;
+        out_buff[3] =
+            output[0].bytes[3].nibbles.high << 4 | output[0].bytes[3].nibbles.low;
 
-      //   out_buff[12] =
-      //       output[3].bytes[0].nibbles.high << 4 | output[3].bytes[0].nibbles.low;
-      //   out_buff[13] =
-      //       output[3].bytes[1].nibbles.high << 4 | output[3].bytes[1].nibbles.low;
-      //   out_buff[14] =
-      //       output[3].bytes[2].nibbles.high << 4 | output[3].bytes[2].nibbles.low;
-      //   out_buff[15] =
-      //       output[3].bytes[3].nibbles.high << 4 | output[3].bytes[3].nibbles.low;
+        out_buff[4] =
+            output[1].bytes[0].nibbles.high << 4 | output[1].bytes[0].nibbles.low;
+        out_buff[5] =
+            output[1].bytes[1].nibbles.high << 4 | output[1].bytes[1].nibbles.low;
+        out_buff[6] =
+            output[1].bytes[2].nibbles.high << 4 | output[1].bytes[2].nibbles.low;
+        out_buff[7] =
+            output[1].bytes[3].nibbles.high << 4 | output[1].bytes[3].nibbles.low;
 
-      //   fwrite(out_buff, 1, 16, oFile);
+        out_buff[8] =
+            output[2].bytes[0].nibbles.high << 4 | output[2].bytes[0].nibbles.low;
+        out_buff[9] =
+            output[2].bytes[1].nibbles.high << 4 | output[2].bytes[1].nibbles.low;
+        out_buff[10] =
+            output[2].bytes[2].nibbles.high << 4 | output[2].bytes[2].nibbles.low;
+        out_buff[11] =
+            output[2].bytes[3].nibbles.high << 4 | output[2].bytes[3].nibbles.low;
 
-      //   input_2[0] = in[0];
-      //   input_2[1] = in[1];
-      //   input_2[2] = in[2];
-      //   input_2[3] = in[3];
-      // }
+        out_buff[12] =
+            output[3].bytes[0].nibbles.high << 4 | output[3].bytes[0].nibbles.low;
+        out_buff[13] =
+            output[3].bytes[1].nibbles.high << 4 | output[3].bytes[1].nibbles.low;
+        out_buff[14] =
+            output[3].bytes[2].nibbles.high << 4 | output[3].bytes[2].nibbles.low;
+        out_buff[15] =
+            output[3].bytes[3].nibbles.high << 4 | output[3].bytes[3].nibbles.low;
 
-      // for (int i = 14; i >= 0; i--) {
-      //   if ((int)out_buff[i] == (int)out_buff[15]) {
-      //     pad_counter++;
-      //   }
-      // }
+        fwrite(out_buff, 1, 16, oFile);
+      }
 
-      // if (pad_counter + 1 == (int)out_buff[15]) {
-      //   fseek(oFile, 0, SEEK_END);       // Seek to the End of the file
-      //   long currentSize = ftell(oFile); // Determine the new File Size
-      //   long newSize = currentSize - (int)out_buff[15];
-      //   ftruncate(fileno(oFile), newSize); // Truncate the File
-      // }
+    for (int i = 14; i >= 0; i--) {
+        if ((int)out_buff[i] == (int)out_buff[15]) {
+          pad_counter++;
+        }
+      }
 
-      // fclose(iFile);
-      // fclose(oFile);
+      if (pad_counter + 1 == (int)out_buff[15]) {
+        fseek(oFile, 0, SEEK_END);       // Seek to the End of the file
+        long currentSize = ftell(oFile); // Determine the new File Size
+        long newSize = currentSize - (int)out_buff[15];
+        ftruncate(fileno(oFile), newSize); // Truncate the File
+      }
 
-      // printf("\n\nDECRYPTED !!!\n");
-
+      fclose(iFile);
+      fclose(oFile);
+      printf("\n\nDECRYPTED !!!\n");
   return 0;
 }
