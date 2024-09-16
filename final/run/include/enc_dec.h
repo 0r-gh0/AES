6be09c25e5b0c10156defa77c879658c5a9dc8a1
Argha <92508807+0r-gh0@users.noreply.github.com>
2024-09-17 01:33:23 +0530

    Update enc_dec.h

--- final/run/include/enc_dec.h
+++ final/run/include/enc_dec.h
@@ -3,7 +3,7 @@
 
 #include "algo.h"  // For AES algorithm functions
 #include "utils.h"  // For utility functions and global sBox
-#include "main.h"  // For HexWord and HexByte data types
+#include "../../main.h"  // For HexWord and HexByte data types
 
 // Function prototypes from enc_dec.c
 void Encrypt(HexWord *in, HexWord *key, HexWord *out);
