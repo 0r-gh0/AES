2b5d970605dd995fba1ac62a6503fa8ae6b3d237
Argha <92508807+0r-gh0@users.noreply.github.com>
2024-09-17 01:42:24 +0530

    Update cbc_enc.h

--- final/modes/include/enc/cbc_enc.h
+++ final/modes/include/enc/cbc_enc.h
@@ -1,10 +1,10 @@
 #ifndef CBC_ENC_H
 #define CBC_ENC_H
 
-#include "main.h"  // For HexWord and data types
-#include "enc_dec.h"  // For encryption functions
-#include "algo.h"
-#include "utils.h"
+#include "../../../main.h"  // For HexWord and data types
+#include "../../../run/include/enc_dec.h"  // For encryption functions
+#include "../../../run/include/algo.h"
+#include "../../../run/include/utils.h"
 
 // Function prototype for CbcEnc
 void CbcEnc(HexWord* input_2, HexWord* keyScheduling, FILE *iFile, FILE *oFile);
