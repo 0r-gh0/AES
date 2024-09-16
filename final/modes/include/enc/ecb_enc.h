306293c8b633ee6ce6838cf02d0830c9c5aa4afa
Argha <92508807+0r-gh0@users.noreply.github.com>
2024-09-17 01:44:41 +0530

    Update ecb_enc.h

--- final/modes/include/enc/ecb_enc.h
+++ final/modes/include/enc/ecb_enc.h
@@ -1,10 +1,10 @@
 #ifndef ECB_ENC_H
 #define ECB_ENC_H
 
-#include "main.h"  // For HexWord and data types
-#include "enc_dec.h"  // For encryption functions
-#include "algo.h"
-#include "utils.h"
+#include "../../../main.h"  // For HexWord and data types
+#include "../../../run/include/enc_dec.h"  // For encryption functions
+#include "../../../run/include/algo.h"
+#include "../../../run/include/utils.h"
 
 // Function prototype for EcbEnc
 void EcbEnc(HexWord* keyScheduling, FILE *iFile, FILE *oFile);
