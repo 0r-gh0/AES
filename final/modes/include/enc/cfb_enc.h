a5005f23d7adf04dab2f06f3fbd988975fa396f0
Argha <92508807+0r-gh0@users.noreply.github.com>
2024-09-17 01:43:53 +0530

    Update cfb_enc.h

--- final/modes/include/enc/cfb_enc.h
+++ final/modes/include/enc/cfb_enc.h
@@ -1,10 +1,10 @@
 #ifndef CFB_ENC_H
 #define CFB_ENC_H
 
-#include "main.h"  // For HexWord and data types
-#include "enc_dec.h"  // For encryption functions
-#include "algo.h"
-#include "utils.h"
+#include "../../../main.h"  // For HexWord and data types
+#include "../../../run/include/enc_dec.h"  // For encryption functions
+#include "../../../run/include/algo.h"
+#include "../../../run/include/utils.h"
 
 // Function prototype for CfbEnc
 void CfbEnc(HexWord* input_2, HexWord* keyScheduling, FILE *iFile, FILE *oFile);
