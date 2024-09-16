ba9d347a02ee31eca9a745f620ce1abc4cbae165
Argha <92508807+0r-gh0@users.noreply.github.com>
2024-09-17 01:45:20 +0530

    Update ofb_enc.h

--- final/modes/include/enc/ofb_enc.h
+++ final/modes/include/enc/ofb_enc.h
@@ -1,10 +1,10 @@
 #ifndef OFB_ENC_H
 #define OFB_ENC_H
 
-#include "main.h"  // For HexWord and data types
-#include "enc_dec.h"  // For encryption functions
-#include "algo.h"
-#include "utils.h"
+#include "../../../main.h"  // For HexWord and data types
+#include "../../../run/include/enc_dec.h"  // For encryption functions
+#include "../../../run/include/algo.h"
+#include "../../../run/include/utils.h"
 
 // Function prototype for OfbEnc
 void OfbEnc(HexWord* input_2, HexWord* keyScheduling, FILE *iFile, FILE *oFile);
