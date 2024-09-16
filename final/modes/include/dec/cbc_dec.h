7313f3395b88242133536c83665d948fe846dcf9
Argha <92508807+0r-gh0@users.noreply.github.com>
2024-09-17 01:46:33 +0530

    Update cbc_dec.h

--- final/modes/include/dec/cbc_dec.h
+++ final/modes/include/dec/cbc_dec.h
@@ -1,10 +1,10 @@
 #ifndef CBC_DEC_H
 #define CBC_DEC_H
 
-#include "main.h"  // For HexWord and data types
-#include "enc_dec.h"  // For encryption functions
-#include "algo.h"
-#include "utils.h"
+#include "../../../main.h"  // For HexWord and data types
+#include "../../../run/include/enc_dec.h"  // For encryption functions
+#include "../../../run/include/algo.h"
+#include "../../../run/include/utils.h"
 
 // Function prototype for CbCDec
 void CbcDec(HexWord* input_2, HexWord* keyScheduling, FILE *iFile, FILE *oFile);
